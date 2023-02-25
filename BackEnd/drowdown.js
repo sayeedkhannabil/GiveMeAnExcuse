aa4cfc66f136c0c889ba2c35a832ea86bff49759
sayeedkhannabil <sayeedkhannabil@gmail.com>
2023-02-25 16:15:02 -0600

    initial done

--- BackEnd/drowdown.js
+++ BackEnd/drowdown.js
@@ -1,7 +1,31 @@
-const arr = ['sick', 'ill', 'dead', 'dogshit'];
-    function newFunction() {
-        var randomNumber = Math.floor(Math.random()*arr.length);
 
-        var mylist = arr[randomNumber];
-        document.getElementById("halum").innerHTML = mylist;
+async function getByType(excuseType) {
+    let url = 'https://excuser-three.vercel.app/v1/excuse/' + excuseType;
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
     }
+}
+
+async function main(excuseType) {
+    let data = await getByType(excuseType);
+    return (data[0].excuse);
+}
+
+
+async function generate() {
+
+    // const arr = ['sick', 'ill', 'dead', 'dogshit'];
+    
+    // var randomNumber = Math.floor(Math.random()*arr.length);
+
+    //var mylist = arr[randomNumber];
+
+    var selection = document.getElementById("mylist").value;
+    let value = await main(selection);
+    //console.log(value);
+    document.getElementById("halum").innerHTML = value;
+    
+}
